25b2b398eefec15040564b5a2fd425865cb7c1f6
upendraabaris <upendra.abaris@gmail.com>
2025-12-20 00:09:08 +0530

    fix vercel

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ 
     error: 'Something went wrong!',
-    message: process.env.NODE_ENV === 'development' ? err.message : 'Internal Server Error'
+    message: process.env.NODE_ENV === 'production' ? err.message : 'Internal Server Error'
   });
 });
 
